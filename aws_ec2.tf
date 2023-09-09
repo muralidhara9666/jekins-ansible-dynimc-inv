47d5d976e7846a7a8a4ed1abe08cf3e542f2afd9
muralidhara9493 <139764522+muralidhara9493@users.noreply.github.com>
2023-09-09 17:08:42 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,14 +1,15 @@
+                
+terraform
+
 provider "aws"{
-region = "ap-south-1"
+region= "ap-south-1"
 }
-
-
-resource "aws_instance" "AWSServer"{
+resource "aws_instance"  "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
+key_name = "dhara"
 tags = {
-Name =  "tomcatservers"
+Name = "tomcaterverss"
 }
 }
