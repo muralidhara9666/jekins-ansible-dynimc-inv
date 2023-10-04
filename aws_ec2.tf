1f87d4b5cbb98612df4d82f46109aeceb343ffb4
muralidhara9493 <139764522+muralidhara9493@users.noreply.github.com>
2023-10-04 19:16:24 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,12 +1,12 @@
 provider "aws"{
 region = "ap-south-1"
 }
-resource "aws_instance" "AWSServer"{
+resource"aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
 key_name = "dhara"
 tags = {
-Name = "tomcaterverss"
+Name = "tomcatervers"
 }
 }
